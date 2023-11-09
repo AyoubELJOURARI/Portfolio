3dc9f9abe0d2e3b83785f1596db963b43cd457da
AyoubELJOURARI <aeljourari@gmail.com>
2023-11-09 17:28:59 +0100

    Implementing a responsive navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { About, Header, Footer, Work, Skills, Testimonial } from './container';
+import {Navbar} from './components';
+import './App.scss';
 
 const App = () => {
 
     return (
-        <div>
-            App
+        <div className='app'>
+            <Navbar/>
+            <Header/>
+            <About/>
+            <Work/>
+            <Skills/>
+            <Testimonial/>
+            <Footer/>
         </div>
     )
 }
